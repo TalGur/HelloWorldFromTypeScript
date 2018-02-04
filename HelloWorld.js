1446a5f23c2348382676d8bd5cced54c82a51991
TalGur <tal.goor@gmail.com>
2018-02-04 16:48:47 +0200

    testing let string | numeric feature

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,15 +2,20 @@ var Startup = /** @class */ (function () {
     function Startup() {
     }
     Startup.main = function () {
-        var msg = GetGreeting();
-        console.log(msg);
+        var msg;
+        msg = GetGreeting(true);
+        console.log('printing numeric: ' + msg);
+        msg = GetGreeting(false);
+        console.log('printing alphabetic: ' + msg);
         return 0;
     };
     return Startup;
 }());
-function GetGreeting() {
-    return 'Greetings from TypeScript!! with build task';
+function GetGreeting(isNumeric) {
+    if (isNumeric)
+        return 12345678;
+    else
+        return 'Greetings from TypeScript!! with build task';
 }
 //https://code.visualstudio.com/docs/languages/typescript
 Startup.main();
-//# sourceMappingURL=HelloWorld.js.map
--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,15 +1,24 @@
 class Startup {
     public static main(): number {
 
-        let msg :string = GetGreeting(); 
-        console.log(msg);
+        let msg :string | number;
+        
+        msg = GetGreeting(true); 
+        console.log('printing numeric: '+ msg);
+
+        msg = GetGreeting(false); 
+        console.log('printing alphabetic: '+msg);
+        
         return 0;
     }
 }
 
-function GetGreeting()
+function GetGreeting(isNumeric )
 {
-    return 'Greetings from TypeScript!! with build task';
+    if (isNumeric)
+        return 12345678;
+    else
+        return 'Greetings from TypeScript!! with build task';
 }
 //https://code.visualstudio.com/docs/languages/typescript
 Startup.main();
