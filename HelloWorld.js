c24b725bef7dc195f2cf6c1045da2b5a492cee57
TalGur <tal.goor@gmail.com>
2018-02-04 15:12:43 +0200

    adding from typescript

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,9 +2,10 @@ var Startup = /** @class */ (function () {
     function Startup() {
     }
     Startup.main = function () {
-        console.log('Hello World');
+        console.log('Hello World from TypeScript!!');
         return 0;
     };
     return Startup;
 }());
+//https://code.visualstudio.com/docs/languages/typescript
 Startup.main();
